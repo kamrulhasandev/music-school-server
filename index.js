3fe96e2dcef759f7dc0d3d0b3ab5a3f12505ed13
Kamrul Hasan <kamrul444216@gmail.com>
2023-06-08 13:33:43 +0600

    is admin and is Inscructor emplement

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -10,6 +11,25 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
 }
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
 app.use(cors(corsOptions))
 app.use(express.json())
 
@@ -28,13 +48,19 @@ async function run() {
   try {
     
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
-
+    
     const usersCollection = client.db('musicSchool').collection('users');
+    
 
-
-
-    // users api
+    // create jwt
+    app.post('/jwt',(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'}) 
+      res.send({token})
+    })
+    
+    
+    // save users api
     app.post('/users', async(req,res)=>{
       const user = req.body;
       const query = {email: user.email}
@@ -46,10 +72,72 @@ async function run() {
       res.send(result)
     })
 
+    // lode user api
+    app.get('/users', async(req,res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
 
+    // user to make admin
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
 
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
 
+    })
 
+    // check user admin or not
+    app.get('/users/admin/:email', verifyJWT, async(req,res)=>{
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        res.send({admin: false})
+      }
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role === 'admin'}
+      res.send(result);
+    })
+
+    // user to instructor
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+    // check user instructor or not
+    app.get('/users/instructor/:email', verifyJWT, async(req,res)=>{
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        res.send({instructor: false})
+      }
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      const result = {instructor: user?.role === 'instructor'}
+      res.send(result);
+    })
+    
+    
+    
+    
+    
+    await client.db('admin').command({ ping: 1 })
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     )
